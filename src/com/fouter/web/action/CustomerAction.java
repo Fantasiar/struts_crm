45213f8166fca8c295f35778c95421d299775fb0
Fouter <1420126933@qq.com>
2018-03-06 19:51:37 +0800

    the second day of struts

--- src/com/fouter/web/action/CustomerAction.java
+++ src/com/fouter/web/action/CustomerAction.java
@@ -13,14 +13,16 @@ import com.fouter.domain.Customer;
 import com.fouter.service.CustomerService;
 import com.fouter.service.impl.CustomerServiceImpl;
 import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.ModelDriven;
 import com.sun.org.apache.regexp.internal.recompile;
 
 import antlr.CSharpCodeGenerator;
 import antlr.StringUtils;
 
-public class CustomerAction extends ActionSupport{
+public class CustomerAction extends ActionSupport implements ModelDriven<Customer>{
 
 	private CustomerService cs=new CustomerServiceImpl();
+	private Customer customer=new Customer();
 
 	public String list() throws Exception {
 		String cust_name=ServletActionContext.getRequest().getParameter("cust_name");
@@ -36,6 +38,24 @@ public class CustomerAction extends ActionSupport{
 		ServletActionContext.getRequest().setAttribute("list", list);
 		
 		return "list";
+	}
+	
+	
+	
+	
+	public String add() throws Exception {
+		// TODO Auto-generated method stub
+		cs.save(customer);
+		
+		return "toList";
+	}
+
+
+
+	@Override
+	public Customer getModel() {
+		// TODO Auto-generated method stub
+		return customer;
 	}
 	
 }
